aec434f133a557b3af431191c9c79ea8b128715b
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-08 14:19:21 +0900

    Exercise 4.7

---
+++ intro/ex4-7.ml
@@ -0,0 +1,15 @@
+(*
+# s;;
+- : ('a -> 'b -> 'c) -> ('a -> 'b) -> 'a -> 'c = <fun>
+# k;; 
+- : 'a -> 'b -> 'a = <fun>
+
+s k k 1
+= k 1 (k 1)
+= 1
+
+k (s k k)
+*)
+let s x y z = x z (y z)
+let k x y = x
+let i = s k k
