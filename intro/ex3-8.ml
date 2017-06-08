aff39046e0a04bce5bc5b351369886f52f0bb6a3
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-08 17:25:08 +0900

    Exercise 3.8

---
+++ intro/ex3-8.ml
@@ -0,0 +1,11 @@
+(*
+ * To calculate x^n, call this function like:
+   # powi x n 1
+   
+   for example:
+   # powi 2 10 1
+   - : int = 1024
+ * *)
+
+let rec powi x n result =
+  if n = 0 then result else powi x (n - 1) (x * result)
