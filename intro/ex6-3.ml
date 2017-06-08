a8f8f0277c9dac666985107e79087d5d07fb6499
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-08 15:31:00 +0900

    Exercise 6.3

---
+++ intro/ex6-3.ml
@@ -0,0 +1,18 @@
+type nat = Zero | OneMoreThan of nat
+
+(* Optional *)
+let rec monus m n =
+  match m with
+    Zero -> if n = Zero then Some Zero else None
+  | OneMoreThan m' ->
+      match n with
+        Zero -> Some m
+      | OneMoreThan n' -> monus m' n'
+
+(* Constants for testing *)
+let zero = Zero
+let one = OneMoreThan zero
+let two = OneMoreThan (one)
+let three = OneMoreThan (two)
+let four = OneMoreThan (three)
+let five = OneMoreThan (four)
