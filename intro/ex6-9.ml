7776e33591c30074d94b5490a72e0bec13becb0c
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-08 16:17:33 +0900

    Exercise 6.9

---
+++ intro/ex6-9.ml
@@ -0,0 +1,16 @@
+type 'a seq = Cons of 'a * (unit -> 'a seq)
+
+let head (Cons (x, _)) = x
+let tail (Cons (_, f)) = f ()
+let rec take n s = if n = 0 then [] else head s :: take (n - 1) (tail s)
+let rec from n = Cons (n, fun () -> from (n + 1))
+
+let rec sift n (Cons (x, f)) =
+  let first = if x mod n = 0 then head (f ()) else x
+  and second = if x mod n = 0 then tail (f ()) else f ()
+  in Cons (first, fun () -> sift n second)
+
+let rec sieve (Cons (x, f)) = Cons (x, fun () -> sieve (sift x (f())))
+let primes = sieve (from 2)
+let rec nthseq n (Cons (x, f)) =
+  if n = 1 then x else nthseq (n - 1) (f())
