d9a585473f99e025a5ae513bdc130feeab2fe806
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-09 10:46:20 +0900

    Exercise 3.2.2
    Catch exceptions when fail to parse/evaluate

--- miniml/main.ml
+++ miniml/main.ml
@@ -4,12 +4,29 @@ open Eval
 let rec read_eval_print env =
   print_string "# ";
   flush stdout;
-  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let (id, newenv, v) = eval_decl env decl in
-    Printf.printf "val %s = " id;
-    pp_val v;
+  try (
+    let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+      let (id, newenv, v) = eval_decl env decl in
+        Printf.printf "val %s = " id;
+        pp_val v;
+        print_newline();
+        read_eval_print newenv
+  ) with
+  Error e -> (
+    Printf.printf "Runtime Error: %s " e;
     print_newline();
-    read_eval_print newenv
+    read_eval_print env
+  )
+  | Failure e -> (
+    Printf.printf "Parse Error: %s " e;
+    print_newline();
+    read_eval_print env
+  )
+  | _ -> (
+    Printf.printf "Syntax Error";
+    print_newline();
+    read_eval_print env
+  )
 
 let initial_env = 
   Environment.extend "i" (IntV 1)
