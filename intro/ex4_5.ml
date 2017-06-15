a254f0ed5ab4f155ed09aea1501847c54e8952d8
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-15 13:59:34 +0900

    Exercise 4.5: Add description

--- intro/ex4_5.ml
+++ intro/ex4_5.ml
@@ -1,6 +1,10 @@
+(* apply function f with n times. *)
 let rec repeat f n x =
   if n > 0 then repeat f (n - 1) (f x) else x
 
+(* apply function s.t. (a, b) -> (b, a + b) with n times
+ * and returns the first element of tuple.
+ * *)
 let fib n =
   let (fibn, _) =
     let next (a, b) = (b, a + b)
