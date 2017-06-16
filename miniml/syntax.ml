d0488c2ee8cc90817e2bb935cf95988ecc7474de
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-16 15:05:03 +0900

    Exercise 4.3.1

--- miniml/syntax.ml
+++ miniml/syntax.ml
@@ -1,11 +1,33 @@
 (* ML interpreter / type reconstruction *)
+type tyvar = int
+
 type ty =
     TyInt
   | TyBool
+  | TyVar of tyvar
+  | TyFun of ty * ty
 
-let pp_ty = function
+let rec pp_ty = function
     TyInt -> print_string "int"
   | TyBool -> print_string "bool"
+  | TyVar _ -> print_string "(unknown)"
+  | TyFun (a, b) ->
+      pp_ty a;
+      print_string " -> ";
+      pp_ty b
+
+let fresh_tyvar =
+  let counter = ref 0 in
+  let body () =
+    let v = !counter in
+      counter := v + 1; v
+  in body
+
+let rec freevar_ty ty = match ty with
+    TyInt -> MySet.empty
+  | TyBool -> MySet.empty
+  | TyVar a -> MySet.singleton a
+  | TyFun (a, b) -> MySet.union (freevar_ty a) (freevar_ty b)
 
 type id = string
 
