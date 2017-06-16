16bbfa398f6346bab76c98bb820f96cdb87e3a8c
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-06-16 09:11:28 +0900

    Exercise 4.1: Add integral sin(x) [0, pi]

--- intro/ex4_1.ml
+++ intro/ex4_1.ml
@@ -1,17 +1,20 @@
-(* generate list of int s.t. [m; m + 1; ...; n] *)
-let rec int_seq m n =
-  if m > n then []
-  else m :: (int_seq (m + 1) n)
-
-let segment f a b n i =
-  let delta = (b -. a) /. (float_of_int n) in
-  (f (a +. (float_of_int (i - 1)) *. delta) +. f (a +. (float_of_int i) *. delta)) *. delta /. 2.
-
-let sum xs = List.fold_left (+.) 0.0 xs
-
+(* integrate f from a to b. *)
 let integral f a b =
-  sum @@ List.map (segment f a b 100) (int_seq 1 100)
+  (* generate list of int s.t. [m; m + 1; ...; n] *)
+  let rec int_seq m n =
+    if m > n then []
+    else m :: (int_seq (m + 1) n)
+  in
+  let sum xs = List.fold_left (+.) 0.0 xs
+  (* Trapezoidal rule. *)
+  and segment f a b n i =
+        let delta = (b -. a) /. (float_of_int n) in
+        (f (a +. (float_of_int (i - 1)) *. delta) +. f (a +. (float_of_int i) *. delta)) *. delta /. 2.
+  in
+    sum @@ List.map (segment f a b 100) (int_seq 1 100)
 
-let pi = 3.14159265359
-
-let sin = Pervasives.sin
+let integrate_sin_from_0_to_pi =
+  let pi = 3.14159265359
+  and sin = Pervasives.sin
+  in
+    integral sin 0. pi
